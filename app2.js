f653f147c5ce4f8f7fee9f8bde33b82245a411da
ignoreandfly <siddhant0701@gmail.com>
2021-09-29 02:16:25 +0530

    bug fixed

--- app2.js
+++ app2.js
@@ -5,17 +5,24 @@ var btn = document.querySelector("#checkbtn");
 var output = document.querySelector("#outputbox");
 
 function clickHandler() {
+
     var ipone = Number(inputone.value);
     var iptwo = Number(inputtwo.value);
     var ipthree = Number(inputthree.value);
-   
+    if (ipone <0 || iptwo<o || ipthree<0){
+        output.innerText="Please enter valid values"
+    }
+   else {
     validator(ipone, iptwo, ipthree);
-
+   }
 }
 
 function validator(ione, itwo, ithree) {
-    var semiperimeter = (ione + itwo + ithree) / 2;
+    if (ione + itwo <= ithree || ione + ithree <= itwo || itwo + ithree <= ione)
+    output.innerText = "These values do not form a triangle"
+else{
+   var semiperimeter = (ione + itwo + ithree) / 2;
     var area = Math.sqrt(semiperimeter * (semiperimeter - ione)*(semiperimeter - itwo)*(semiperimeter - ithree));
-    output.innerText = "Area: "+ area
+    output.innerText = "Area: "+ area}
 }
 btn.addEventListener("click", clickHandler)
